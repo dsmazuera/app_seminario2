7aa86160943185b68eb08a1f0663fd80a9790366
dsmazuera <diana.mazuera1@u.icesi.edu.co>
2026-04-25 11:41:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
+st.write("This is my first Streamlit application.")
+st.success("The app is running correctly.")
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
@@ -17,7 +19,7 @@ with st.form('my_form'):
     submitted = st.form_submit_button('Submit')
 
     if not openai_api_key.startswith('sk-'):
-        st.warning('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', icon='⚠️')
+        st.warning('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', icon='⚠️')
 
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
