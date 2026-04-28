5bcdf050b9b1fd8e6ff8b878ad5bbb5fec5f2be0
dsmazuera <diana.mazuera1@u.icesi.edu.co>
2026-04-27 23:00:07 -0500

    Actualización

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
 
 st.title('🦜🔗 Quickstart App')
+
 st.write("This is my first Streamlit application.")
 st.success("The app is running correctly.")
 
@@ -9,17 +10,13 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    # Cambiamos llm(input_text) por llm.invoke(input_text)
+    st.info(llm.invoke(input_text))
 
 with st.form('my_form'):
-    text = st.text_area(
-        'Enter text:',
-        'What are the three key pieces of advice for learning how to code?'
-    )
-    submitted = st.form_submit_button('Submit')
-
-    if not openai_api_key.startswith('sk-'):
-        st.warning('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', icon='⚠️')
-
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
